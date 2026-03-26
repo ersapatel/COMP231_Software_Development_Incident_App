12db810d11e6833010058529d0f5f97280ead96f
Swapnil Patel <ersapatel@gmail.com>
2026-03-26 14:13:48 -0400

    Update mongoUri for database connection

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
-    mongoUri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/incidentapp'
+    mongoUri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/codecrafters'
 };
 
 export default config;
